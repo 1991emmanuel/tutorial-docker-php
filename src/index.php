639ff41f541b3a98de3900e0458180a3f967e59c
manolog <emmanuel.ganster@gmail.com>
2021-02-15 14:00:38 -0600

    se hace un ultimo cambio en el archivo index.php 15/02/2021

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
 <?php
-   echo("Hola a todo el Mundo 11 de febrero de 2021 se hace un ultimo cambio");
+   echo("Hola a todo el Mundo 15 de febrero de 2021 se hace un ultimo cambio");
     phpinfo();
 ?>
